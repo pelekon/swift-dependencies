61bc71059d3483509f67f05bcedf5d2fbf10f329
bartlomiej.bukowieck <pelekon@gmail.com>
2024-05-30 18:32:37 +0200

    Update package with local dependency for precompiled SwiftSyntax usage in TCA.

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -22,7 +22,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"511.0.0"),
+    .package(url: "https://github.com/sjavora/swift-syntax-xcframeworks.git", from: "509.0.2"),
     .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.0"),
@@ -62,8 +62,7 @@ let package = Package(
     .macro(
       name: "DependenciesMacrosPlugin",
       dependencies: [
-        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+        .product(name: "SwiftSyntaxWrapper", package: "swift-syntax-xcframeworks")
       ]
     ),
     .executableTarget(
